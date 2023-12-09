d1b15715be53f489e81aae630ea68f5ac88b6ef9
Miryam Levi <n8670920@gmail.com>
2023-12-10 01:29:53 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export function Home() {
+    return (
+        <section className="home">
+            <h1>Welcome to our React App</h1>
+            {/* <img src={imgUrl} alt="" /> */}
+        </section>
+    )
+}
