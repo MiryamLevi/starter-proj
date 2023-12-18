6acb3c29accdd8bd9418125fdf1d8944029fad6a
Miryam Levi <n8670920@gmail.com>
2023-12-18 15:01:09 +0200

    show content in the emails list

--- src/cmps/EmailList.jsx
+++ src/cmps/EmailList.jsx
@@ -1,9 +1,13 @@
+import { EmailPreview } from "./EmailPreview";
+
 export function EmailList({ emails }) {
   return (
-    <>
-      {emails.map((email) => {
-        <li>{email.title} | email.time </li>;
-      })}
-    </>
+    <ul className="email-list">
+      {emails.map((email) => (
+        <li key={email.key}>
+          <EmailPreview email={email} />;
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { EmailIndex } from './EmailIndex'
-import {Menu} from '../cmps/Menu'
+import { EmailIndex } from "./EmailIndex";
+import { Menu } from "../cmps/Menu";
 export function Home() {
-    return (
-        <section className="home">
-            <Menu/>
-            <article className="menu-item-content"></article>
-            <EmailIndex/>
-        </section>
-    )
+  return (
+    <section className="home">
+      <Menu />
+      <article className="menu-item-content">
+        <EmailIndex />
+      </article>
+    </section>
+  );
 }
